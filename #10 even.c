f2cff5757493bd7a6d155e243e758cb6a7c228cc
Aashish Bedi <120207125+AashishBedi@users.noreply.github.com>
2023-06-22 15:53:01 +0530

    Add files via upload

---
+++ #10 even.c
@@ -0,0 +1,12 @@
+// WAP to check whether a number is divisible by 2 or not without using if-else
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	printf("a: ");
+	scanf("%d",&a);
+	printf("%d \n", a%2 == 0);
+	return 0;
+}
